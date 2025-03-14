b371026bf74f03773c9417a76aaabcb4039a1455
SwixShop <165885283+SwixShop@users.noreply.github.com>
2025-03-14 23:45:58 +1000

    Create crypt_V2.py

---
+++ crypt_V2.py
@@ -0,0 +1,41 @@
+symbols = [i for i in "puёрog9G^lф`(ИФПJДBЖAяM@._CУ+&ЩбМESВЕ#ЬWЫmезI-п№2dLyqАwNЦ>йtjв}vkн~у=x)чсKиie,Б0О[Oa1QхzU ЁЙь3дг4шЗ|YЛ$ъжsкСZтРVН!;/о8PЯ5HаnDы7ЮГ{КcЭм*л:<b6ШXf%hщюTrТЧЪ]эRFХц"]
+
+def crypt(text: str, key: str, lang: str = "ru", sdvig: int = 0) -> str:
+    """lang: ru, en """
+    string = ""
+    before = 0
+    for symbol in text.lower():
+        if symbol in symbols:
+            string += symbols[(symbols.index(symbol) + before + sdvig) % len(symbols)]
+            try:
+                before += symbols.index(symbol) + \
+                    int(sdvig ** 0.5) * sdvig + \
+                    sdvig * symbols.index(symbol) ** 3 * int(before ** 0.5) + \
+                    key.index(key[before % len(key)]) ** 2 * before
+            except:
+                before = 25
+        else:
+            string += symbol
+    return string
+
+
+def decrypt(text: str, key: str, lang: str = "ru", sdvig: int = 0) -> str:
+    """lang: ru, en """
+    string = ""
+    before = 0
+    for symbol in text:
+        if symbol in symbols:
+            string += symbols[(symbols.index(symbol) - before - sdvig) % len(symbols)]
+            try:
+                before += symbols.index(symbols[(symbols.index(symbol) - before - sdvig) % len(symbols)]) + \
+                    int(sdvig ** 0.5) * sdvig + \
+                    sdvig * symbols.index(symbols[(symbols.index(symbol) - before - sdvig) % len(symbols)]) ** 3 * int(before ** 0.5) + \
+                    key.index(key[before % len(key)]) ** 2 * before
+            except:
+                before = 25
+        else:
+            string += symbol
+    return string.capitalize()
+
+print(crypt("Генератор текста — компьютерная программа, генерирующая тексты, корректные с точки зрения большинства языковых норм, но, как правило, лишённые смысла.", "костыли на корточках", "ru", 34345))
+print(decrypt("RyЛVNX8 лQ+к1CВr6—ПЗйбщЫ@XЧ&NЙ,2xg`umЕN6u +ЧЛ&Уу/nh№бМE9кУ~P>йZ~NqПК0kFйжuрИ№=[ВШRД$с3>щуЫhlnHvсaК62KEР5Б`сГЗyвЯH09pОЁicr|(к0HТя<GzbЦх1u[ncx(sв>|qGnОщ", "костыли на корточках", "ru", 34345))
